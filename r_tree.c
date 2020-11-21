f0c465cd3ada9f234aaa9646a893e942d9bfee5c
Xinyue211 <xzhang4@andrew.cmu.edu>
2020-11-21 15:26:50 -0500

    Implemented object insert

--- r_tree.c
+++ r_tree.c
@@ -5,6 +5,18 @@
 #include "r_tree.h"
 #include "util.h"
 
+r_tree_t* init_rtree() {
+    node_t* root = (node_t*) malloc(sizeof(node_t));
+    root->node_type = LEAF;
+    root->rectangle = init(0, 0, 0, 0);
+    root->node_size = 0;
+
+    r_tree_t* r_tree = (r_tree_t*) malloc(sizeof(r_tree_t));
+    r_tree->root = root;
+
+    return r_tree;
+}
+
 size_t search(node_t *node, rectangle_t area, object_t ***objects) {
   if (node == NULL) {
     *objects = NULL;
@@ -59,16 +71,20 @@ size_t search(node_t *node, rectangle_t area, object_t ***objects) {
 
 void insert(node_t *root, object_t *object, rectangle_t o_area) {
     choose_and_insert(root, NULL, o_area, object);
+//    printf("\nRTREE\n");
+//    print_tree(root, 0);
 }
 
 node_t* choose_and_insert(node_t* node, node_t* parent, rectangle_t o_area, object_t* object) {
     if (node->node_type == LEAF) {
-        node_t* new_obj = (node_t*) malloc(sizeof(object_t));
+        node_t* new_obj = (node_t*) malloc(sizeof(node_t));
         new_obj->node_type = OBJECT;
         new_obj->rectangle = o_area;
         new_obj->object = object;
-        node->entries[node->node_size] = new_obj;
-        node->node_size++;
+        node->entries[node->node_size++] = new_obj;
+        if (node->node_size == 1) {
+            node->rectangle = o_area;
+        }
 
     } else {
         int min_area = 0;
@@ -87,15 +103,14 @@ node_t* choose_and_insert(node_t* node, node_t* parent, rectangle_t o_area, obje
             new_node->node_type = LEAF;
             new_node->node_size = 0;
             new_node->rectangle = o_area;
-            node->entries[0] = new_node;
-            node->node_size++;
+            node->entries[node->node_size++] = new_node;
+            node->node_type = INTERNAL;
             min_idx = 0;
         }
 
         node_t* res = choose_and_insert(node->entries[min_idx], node, o_area, object);
         o_area = res->rectangle;
     }
-
 
     node->rectangle = cover(node->rectangle, o_area);
 
@@ -107,11 +122,158 @@ node_t* choose_and_insert(node_t* node, node_t* parent, rectangle_t o_area, obje
 }
 
 void split_node(node_t* node, node_t* parent) {
+    int top = 0, top_idx = -1;
+    int bottom = 0, bottom_idx = -1;
+    int right = 0, right_idx = -1;
+    int left = 0, left_idx = -1;
+    long total_height = 0;
+    long total_width = 0;
 
+    size_t size = node->node_size;
+    int i;
+    for (i = 0; i < size; i++) {
+        node_t* curr_n = node->entries[i];
+        total_height += (curr_n->rectangle.top_right.y - curr_n->rectangle.bottom_left.y);
+        total_width += (curr_n->rectangle.top_right.x - curr_n->rectangle.bottom_left.x);
+
+        if (top_idx == -1 || top < curr_n->rectangle.top_right.y) {
+            top = curr_n->rectangle.top_right.y;
+            top_idx = i;
+        }
+        if (bottom_idx == -1 || bottom > curr_n->rectangle.bottom_left.y) {
+            bottom = curr_n->rectangle.bottom_left.y;
+            bottom_idx = i;
+        }
+        if (right_idx == -1 || right < curr_n->rectangle.top_right.x) {
+            right = curr_n->rectangle.top_right.x;
+            right_idx = i;
+        }
+        if (left_idx == -1 || left > curr_n->rectangle.bottom_left.x) {
+            left = curr_n->rectangle.bottom_left.x;
+            left_idx = i;
+        }
+    }
+
+    int group1, group2;
+    if (right_idx == left_idx || ((top-bottom)/total_height>(right-left)/total_width && top_idx != bottom_idx)) {
+        group1 = bottom_idx;
+        group2 = top_idx;
+    } else {
+        group1 = left_idx;
+        group2 = right_idx;
+    }
+
+    node_t* node2 = (node_t*) malloc(sizeof(node_t));
+    node2->node_type = node->node_type;
+    node2->entries[0] = node->entries[group2];
+    node2->node_size = 1;
+    node2->rectangle = node->entries[group2]->rectangle;
+    node->rectangle = node->entries[group1]->rectangle;
+    node->node_size = 0;
+
+    node_t* remain = NULL;
+    for (i = 0; i < size; i++) {
+        if (i == group2) {
+            continue;
+        }
+        if (i == group1) {
+//            printf("%zu %zu %zu %zu assigned to node\n",
+//                    node->entries[i]->rectangle.bottom_left.x, node->entries[i]->rectangle.bottom_left.y,
+//                   node->entries[i]->rectangle.top_right.x, node->entries[i]->rectangle.top_right.y);
+            node->entries[node->node_size++] = node->entries[i];
+            continue;
+        }
+        if (node->node_size == MIN_ENTRY_SIZE && node2->node_size < MIN_ENTRY_SIZE) {
+            remain = node2;
+            break;
+        }
+        if (node2->node_size == MIN_ENTRY_SIZE && node->node_size < MIN_ENTRY_SIZE) {
+            remain = node;
+            break;
+        }
+
+        rectangle_t r1 = cover(node->rectangle, node->entries[i]->rectangle);
+        int diff1 = area_diff(r1, node->rectangle);
+        rectangle_t r2 = cover(node2->rectangle, node->entries[i]->rectangle);
+        int diff2 = area_diff(r2, node2->rectangle);
+
+        if (diff1 < diff2) {
+            node->rectangle = r1;
+            node->entries[node->node_size++] = node->entries[i];
+        } else {
+            node2->rectangle = r2;
+            node2->entries[node2->node_size++] = node->entries[i];
+        }
+    }
+    for (; i < size; i++) {
+        remain->rectangle = cover(remain->rectangle, node->entries[i]->rectangle);
+        remain->entries[remain->node_size++] = node->entries[i];
+    }
+
+    if (parent != NULL) {
+        parent->entries[parent->node_size++] = node2;
+        parent->rectangle = cover(parent->rectangle, node2->rectangle);
+    } else {
+        node_t* node1 = (node_t*) malloc(sizeof(node_t));
+        copy_node(node, node1);
+        node->node_type = INTERNAL;
+        node->node_size = 2;
+        node->rectangle = cover(node1->rectangle, node2->rectangle);
+        node->entries[0] = node1;
+        node->entries[1] = node2;
+    }
+}
+
+void copy_node(node_t* src, node_t* dest) {
+    dest->node_type = src->node_type;
+    dest->node_size = src->node_size;
+    dest->rectangle = src->rectangle;
+    dest->object = src->object;
+    int i;
+    for (i = 0; i < src->node_size; i++) {
+        dest->entries[i] = src->entries[i];
+    }
+}
+
+void print_tree(node_t* node, int level) {
+     if (node->node_type == OBJECT) {
+         printf("Level: %d Object: %zu %zu %zu %zu\n", level, node->rectangle.bottom_left.x, node->rectangle.bottom_left.y,
+                 node->rectangle.top_right.x, node->rectangle.top_right.y);
+     } else {
+         int i;
+         for (i = 0; i < node->node_size; i++) {
+             print_tree(node->entries[i], level + 1);
+         }
+         if (node->node_type == INTERNAL) {
+             printf("Level: %d Internal: ", level);
+         } else {
+             printf("Level: %d Leaf: ", level);
+         }
+         printf("%zu %zu %zu %zu\n", node->rectangle.bottom_left.x, node->rectangle.bottom_left.y,
+                node->rectangle.top_right.x, node->rectangle.top_right.y);
+     }
+}
+
+void free_rtree(r_tree_t* r_tree) {
+    free_node(r_tree->root);
+    free(r_tree);
+}
+
+void free_node(node_t* node) {
+    if (node->node_type == OBJECT) {
+//        free(node->object);
+    } else {
+        int i;
+        for (i = 0; i < node->node_size; i++) {
+            free_node(node->entries[i]);
+        }
+    }
+
+    free(node);
 }
 
 int main() {
-  printf("test basic search\n");
+  printf("test basic search&insert\n");
 
   object_t objects[8];
   node_t object_nodes[8];
@@ -132,46 +294,53 @@ int main() {
   object_nodes[6].rectangle = init(3, 5, 4, 6);
   object_nodes[7].rectangle = init(5, 5, 6, 6);
 
-  node_t leaf_nodes[4];
-  memset(leaf_nodes, 0, 4 * sizeof(node_t));
+//  node_t leaf_nodes[4];
+//  memset(leaf_nodes, 0, 4 * sizeof(node_t));
+//
+//  for (int i = 0; i < 4; i++) {
+//    leaf_nodes[i].node_type = LEAF;
+//    leaf_nodes[i].node_size = 2;
+//    leaf_nodes[i].entries[0] = &(object_nodes[2 * i]);
+//    leaf_nodes[i].entries[1] = &(object_nodes[2 * i + 1]);
+//    leaf_nodes[i].rectangle = cover(leaf_nodes[i].entries[0]->rectangle, leaf_nodes[i].entries[1]->rectangle);
+//  }
+//
+//  node_t internal_nodes[2];
+//  memset(internal_nodes, 0, 2 * sizeof(node_t));
+//
+//  for (int i = 0; i < 2; i++) {
+//    internal_nodes[i].node_type = INTERNAL;
+//    internal_nodes[i].node_size = 2;
+//    internal_nodes[i].entries[0] = &(leaf_nodes[2 * i]);
+//    internal_nodes[i].entries[1] = &(leaf_nodes[2 * i + 1]);
+//    internal_nodes[i].rectangle = cover(internal_nodes[i].entries[0]->rectangle, internal_nodes[i].entries[1]->rectangle);
+//  }
 
-  for (int i = 0; i < 4; i++) {
-    leaf_nodes[i].node_type = LEAF;
-    leaf_nodes[i].node_size = 2;
-    leaf_nodes[i].entries[0] = &(object_nodes[2 * i]);
-    leaf_nodes[i].entries[1] = &(object_nodes[2 * i + 1]);
-    leaf_nodes[i].rectangle = cover(leaf_nodes[i].entries[0]->rectangle, leaf_nodes[i].entries[1]->rectangle);
-  }
-
-  node_t internal_nodes[2];
-  memset(internal_nodes, 0, 2 * sizeof(node_t));
-
-  for (int i = 0; i < 2; i++) {
-    internal_nodes[i].node_type = INTERNAL;
-    internal_nodes[i].node_size = 2;
-    internal_nodes[i].entries[0] = &(leaf_nodes[2 * i]);
-    internal_nodes[i].entries[1] = &(leaf_nodes[2 * i + 1]);
-    internal_nodes[i].rectangle = cover(internal_nodes[i].entries[0]->rectangle, internal_nodes[i].entries[1]->rectangle);
-  }
-
-  node_t root;
-
-  root.node_type = INTERNAL;
-  root.node_size = 2;
-  root.entries[0] = &(internal_nodes[0]);
-  root.entries[1] = &(internal_nodes[1]);
-  root.rectangle = cover(root.entries[0]->rectangle, root.entries[1]->rectangle);
-
-  r_tree_t r_tree;
-  r_tree.root = &root;
+//  node_t root;
+//
+//  root.node_type = INTERNAL;
+//  root.node_size = 2;
+//  root.entries[0] = &(internal_nodes[0]);
+//  root.entries[1] = &(internal_nodes[1]);
+//  root.rectangle = cover(root.entries[0]->rectangle, root.entries[1]->rectangle);
+//
+//  r_tree_t r_tree;
+//  r_tree.root = &root;
 
   object_t **object_pointer = NULL;
 
-  size_t count = search(r_tree.root, init(3, 5, 6, 6), &object_pointer);
+  r_tree_t* r_tree = init_rtree();
+
+  for (int i = 0; i < 8; i++) {
+      insert(r_tree->root, object_nodes[i].object, object_nodes[i].rectangle);
+  }
+
+  size_t count = search(r_tree->root, init(3, 5, 6, 6), &object_pointer);
 
   printf("Object Founded:\n");
   for (int i = 0; i < count; i++) {
     printf("Object Id : %ld\n", object_pointer[i]->id);
   }
   free(object_pointer);
+  free_rtree(r_tree);
 }
--- r_tree.h
+++ r_tree.h
@@ -7,7 +7,8 @@
 
 #include "util.h"
 
-#define MAX_ENTRY_SIZE 10
+#define MAX_ENTRY_SIZE 2
+#define MIN_ENTRY_SIZE 1
 
 #define INTERNAL 0
 #define LEAF 1
@@ -25,9 +26,15 @@ typedef struct {
   node_t *root;
 }r_tree_t;
 
+r_tree_t* init_rtree();
 size_t search(node_t *node, rectangle_t area, object_t ***objects);
-void insert(node_t *root, object_t *object, rectangle_t o_area); // return 0 if insert successfully
+void insert(node_t *root, object_t *object, rectangle_t o_area);
+
 node_t* choose_and_insert(node_t* node, node_t* parent, rectangle_t o_area, object_t* object);
 void split_node(node_t* node, node_t* parent);
+void copy_node(node_t* src, node_t* dest);
+void print_tree(node_t* node, int level);
+void free_rtree(r_tree_t* r_tree);
+void free_node(node_t* root);
 
 #endif //INFECTIOUSDISEASEMODELING_R_TREE_H
